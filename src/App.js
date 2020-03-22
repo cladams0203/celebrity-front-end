78d9b226c238992c93870ab53fb4451f21a813d4
cladams0203 <cladams0203@gmail.com>
2020-03-22 15:40:58 -0400

    added login component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 
 import { Register }from './components/Register'
 import { CelebContainer } from './components/CelebContainer'
+import { Login } from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      {/* <Register /> */}
+      <Login />
       <CelebContainer />
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import api from '../utils/api'
 
 export function Login() {
 
+    const [form, setForm] = useState({
+        username: '',
+        password: ''
+    })
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        api().post('/users/login', form)
+            .then(res => {
+                console.log(res)
+                localStorage.setItem('token', res.data.token)
+            })
+            .catch(err => console.log(err))
+    }
+    const handleChange = (e) => {
+        setForm({...form, [e.target.name]: e.target.value})
+    }
     return (
-        <form>
-            <input />
+        <form onSubmit={(e) => handleSubmit(e)}>
+            <input
+                name='username'
+                value={form.username}
+                placeholder='Username'
+                onChange={(e) => handleChange(e)}
+            />
+            <input
+                name='password'
+                value={form.password}
+                placeholder='Password'
+                onChange={(e) => handleChange(e)}
+                type='password'
+            />
+            <button type='submit'>Login</button>
         </form>
     )
 }
